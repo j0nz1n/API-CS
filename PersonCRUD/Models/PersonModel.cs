3f066e295ebc8696ed7417080b810e8fe0b82241
Jônatas Pereira dos Santos <ticonunespereira@gmail.com>
2026-03-20 12:35:52 -0300

    CRUD Working

--- PersonCRUD/Models/PersonModel.cs
+++ PersonCRUD/Models/PersonModel.cs
@@ -4,6 +4,14 @@ public class PersonModel
 {
     public Guid Id { get; init; }
     public string Name {get; private set;}
+    public void ChangeName(string name)
+    {
+        Name = name;
+    }
+    public void SetInactive()
+    {
+        Name = "Desativado";
+    }
     public PersonModel(string name)
     {
         Name = name;
--- PersonCRUD/Routes/PersonRoute.cs
+++ PersonCRUD/Routes/PersonRoute.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Person.Data;
 using Person.Models;
 
 namespace Person.Routes;
@@ -6,7 +8,42 @@ public static class PersonRoute
 {
     public static void PersonRoutes( this WebApplication app)
     {
-        app.MapGet("person", () => new PersonModel("Jon"));
-        // post, put, delete, patch
+        var route = app.MapGroup("person");
+
+        route.MapPost("", async (PersonRequest req, PersonContext context) =>
+        {
+            var person = new PersonModel(req.name);
+            await context.AddAsync(person);
+            await context.SaveChangesAsync();
+        });
+
+        route.MapGet("", async (PersonContext context) =>
+        {
+            var people = await context.People.ToListAsync();
+            return Results.Ok(people);
+        });
+
+        route.MapPut("{id:guid}", async (Guid id, PersonRequest req, PersonContext context) =>
+        {
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(person == null)
+                return Results.NotFound();
+            
+            person.ChangeName(req.name);
+            await context.SaveChangesAsync();
+            return Results.Ok(person);
+        });
+        route.MapDelete("{id:guid}",async(Guid id, PersonContext context) =>
+        {
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(person == null)
+                return Results.NotFound();
+
+            person.SetInactive();
+            await context.SaveChangesAsync();
+            return Results.Ok(person);
+        });
     }
 }
