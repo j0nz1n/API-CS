301b6be4366954219eb6fd765301493121e93d18
Jônatas Pereira dos Santos <ticonunespereira@gmail.com>
2026-03-21 10:54:28 -0300

    Deleting Comments

--- PersonCRUD/Program.cs
+++ PersonCRUD/Program.cs
@@ -3,15 +3,12 @@ using Person.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<PersonContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
